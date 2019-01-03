f09af3fc31438fcb82d697a1579b120c7882a76f
duho wise <wduho@hotmail.com>
2019-01-03 23:43:42 +0000

    Initial Commit

---
+++ MessageQueueingDemo.Subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MessageQueueingDemo.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost"
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+          channel.QueueDeclare("MessageQueueingDemoQueue", true, false, false, null);
+
+          var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model,eventArgs)=>
+            {
+                var body = eventArgs.Body;
+                Console.WriteLine($"Received Message: {Encoding.UTF8.GetString(body)}");
+                Thread.Sleep(100);
+                channel.BasicAck(eventArgs.DeliveryTag,false);
+            };
+
+            channel.BasicConsume( "MessageQueueingDemoQueue",  false, consumer);
+
+            Console.WriteLine("now waiting for messages.......");
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
--- MessageQueueingDemo/Program.cs
+++ MessageQueueingDemo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 
 namespace MessageQueueingDemo
 {
@@ -6,7 +9,54 @@ namespace MessageQueueingDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //using (var bus=RabbitHutch.CreateBus("localhost"))
+            //{
+            //    Console.WriteLine("Hello World!");
+            //}
+
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost"
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            var properties = channel.CreateBasicProperties();
+            properties.DeliveryMode = 2;
+
+            channel.ExchangeDeclare("MessageQueueingDemoExchange",ExchangeType.Direct);
+            channel.QueueDeclare("MessageQueueingDemoQueue", true, false, false, null);
+            channel.QueueBind("MessageQueueingDemoQueue", "MessageQueueingDemoExchange","RoutingKey",null);
+
+            while (true)
+            {
+                var message = new MyMessage
+                {
+                    Name =$"Duhp{new Random().Next(10, 50)}",
+                    Address = "56 Independence Avenue",
+                    ShoeSize =new Random().Next(10,50)
+                };
+                var messageString = JsonConvert.SerializeObject(message);
+                var messageBytes = Encoding.UTF8.GetBytes(messageString);
+                channel.BasicPublish("MessageQueueingDemoExchange", "RoutingKey",null, messageBytes);
+                Console.WriteLine($"published {message}");
+            }
+            
+
+            channel.Dispose();
+            connection.Dispose();
+            Console.ReadKey();
+
+
         }
+    }
+
+    class MyMessage
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ShoeSize { get; set; }
     }
 }
